88308b5ca64d1812d81a573ada8f1ed3e987debf
Rubens.Marcelo <Rubens.Marcelo@tiberio.com.br>
2024-09-20 09:48:03 -0300

    ver0.0.02

---
+++ userwidgets/com.konymp.Header/modules/require/HeaderControllerActions.js
@@ -0,0 +1,11 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onTouchStart defined for ImgClose **/
+    AS_Image_b3cbd928ba8f4682b6f2ebd99328c76b: function AS_Image_b3cbd928ba8f4682b6f2ebd99328c76b(eventobject, x, y) {
+        var self = this;
+        this.view.isVisible = false;
+        this.view.forceLayout();
+    }
+});
